7134cd7f0c6e60393a6d977f01b3d63e12c234ca
Min Lwin <lwin.zawmin@gmail.com>
2024-03-23 15:54:38 +0630

    Replase new angular project

--- 06-angular/rest-security-web/src/app/app.routes.ts
+++ 06-angular/rest-security-web/src/app/app.routes.ts
@@ -1,9 +1,3 @@
 import { Routes } from '@angular/router';
-import { UserHomeComponent } from './pages/user-home/user-home.component';
-import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [
-  {path: 'home', component: UserHomeComponent},
-  {path: 'login', component: LoginComponent},
-  {path: '', redirectTo: '/home', pathMatch: 'full'}
-];
+export const routes: Routes = [];
